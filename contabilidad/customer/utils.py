708d9a7e93b72d14f89d3821f12d05bd40b55476
Ricardo Tercero Solis <ricardo.tercero@r3s.com.mx>
2018-03-28 11:40:24 -0600

    Add RFC validator

---
+++ contabilidad/customer/utils.py
@@ -0,0 +1,8 @@
+import re
+
+
+def validate_rfc(rfc):
+    pattern = "^([a-zA-Z]{3,4})([0-9]{6})([a-zA-Z0-9]{3})$"
+    compile = re.compile(pattern=pattern, flags=re.I)
+    result = compile.fullmatch(rfc)
+    return result.string == rfc
