8f0e353ac2a2aa9c415d3171c882163e3b8a4f39
Ricardo Tercero Solis <ricardo.tercero@r3s.com.mx>
2018-04-18 10:59:21 -0500

    Add property to return membership

--- contabilidad/customer/models.py
+++ contabilidad/customer/models.py
@@ -9,11 +9,15 @@ class Company(models.Model):
     full_name = models.CharField(max_length=250, null=False, blank=False, verbose_name=_('Company name'))
     rfc = models.CharField(max_length=13, name=False, blank=False, unique=True, verbose_name=_('Tax ID'),
                            help_text=_('Use the following format XXX[X]999999XXX'))
-    address = JSONField(default={})
+    address = JSONField(default={}, null=True)
     collaborators = models.ManyToManyField(User, related_name='companies', verbose_name=_('Collaborators'))
     has_membership = models.BooleanField(default=False)
     membership_has_expired = models.BooleanField(default=False)
     expiration_date = models.DateField(null=True, default=None)
+
+    @property
+    def membership_request(self):
+        return None
 
     class Meta:
         ordering = ['rfc']
