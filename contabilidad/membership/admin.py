aeb791c380c1c8b1090833fdae52993fa2011ace
Ricardo Tercero Solis <ricardo.tercero@r3s.com.mx>
2018-04-18 22:06:39 -0500

    Add state and municipality model to admin

--- contabilidad/membership/admin.py
+++ contabilidad/membership/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
-from .models import MembershipRequest, Member
+from .models import MembershipRequest, Member, State, Municipality
 
 
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(MembershipRequest)
 class MembershipRequestAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(State)
+class StateAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Municipality)
+class MunicipalityAdmin(admin.ModelAdmin):
     pass
