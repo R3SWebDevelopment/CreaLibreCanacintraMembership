8a540d95ebe2b1c9b0821689fddd23b8d388585d
Ricardo Tercero Solis <ricardo.tercero@r3s.com.mx>
2018-04-23 21:43:23 -0500

    Add scian and tariff traffic point to catalog end point

--- contabilidad/utils/api/serializers.py
+++ contabilidad/utils/api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from membership.models import Sector, State
-from membership.api.serializers import StateSerializer, SectorSerializer
+from membership.models import Sector, State, SCIAN, TariffFraction
+from membership.api.serializers import StateSerializer, SectorSerializer, SCIANSerializer, TariffFractionSerializer
 from datetime import datetime
 from django.conf import settings
 import pytz
@@ -31,9 +31,18 @@ class HealthCheckSerializer(serializers.Serializer):
 class CatalogSerializer(serializers.Serializer):
     states = serializers.SerializerMethodField()
     sectors = serializers.SerializerMethodField()
+    scian = serializers.SerializerMethodField()
+    tariff_traffic = serializers.SerializerMethodField()
 
     def get_states(self, obj):
         return StateSerializer(State.objects.all(), many=True).data
 
     def get_sectors(self, obj):
         return SectorSerializer(Sector.objects.all(), many=True).data
+
+    def get_scian(self, obj):
+        return SCIANSerializer(SCIAN.objects.all(), many=True).data
+
+    def get_tariff_traffic(self, obj):
+        return TariffFractionSerializer(TariffFraction.objects.all(), many=True).data
+
