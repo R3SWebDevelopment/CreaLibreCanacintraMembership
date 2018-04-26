1520cd29b913073004e00c314f3751f2c4a56732
Ricardo Tercero <ricardo.tercero@vordem.mx>
2018-04-26 16:05:34 -0500

    Add membership request attachment files end point

--- contabilidad/membership/api/urls.py
+++ contabilidad/membership/api/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import MyMembershipRequestView, StateView, SectorView, SCIANView, TariffFractionView
+from .views import MyMembershipRequestView, StateView, SectorView, SCIANView, TariffFractionView, \
+    MyMembershipRequestAttachmentView
 
 urlpatterns = [
     url(r'^$', MyMembershipRequestView.as_view()),
+    url(r'^request/attachment/$', MyMembershipRequestAttachmentView.as_view()),
     url(r'^states/$', StateView.as_view()),
     url(r'^sector/$', SectorView.as_view()),
     url(r'^scian/$', SCIANView.as_view()),
