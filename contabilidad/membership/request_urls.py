583f7ab3f5eac9aea659f3873e1a6e7e805a6d2f
Ricardo Tercero <ricardo.tercero@vordem.mx>
2018-04-27 23:28:26 -0500

    Add view to render attachment image to pdf

--- contabilidad/membership/request_urls.py
+++ contabilidad/membership/request_urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
 
 from .views import (
-    GeneratePDF
+    GeneratePDF, ViewAttachmentPDF
 )
 
 
 urlpatterns = [
-    url(r'^form_pdf/$', GeneratePDF.as_view(), name='generate_membership_request')
+    url(r'^form_pdf/$', GeneratePDF.as_view(), name='generate_membership_request'),
+    url(r'^attachment/view/$', ViewAttachmentPDF.as_view(), name='generate_membership_request')
 ]
--- contabilidad/membership/views.py
+++ contabilidad/membership/views.py
@@ -1,8 +1,31 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
 from django.views.generic import View
-from .models import MembershipRequest
+from .models import MembershipRequest, AttachedFile
 from utils.render_to_pdf import render_to_pdf
+
+
+class ViewAttachmentPDF(View):
+    def get(self, request, *args, **kwargs):
+        attachment_id = request.GET.get('id', None)
+        if attachment_id:
+            attachment = AttachedFile.objects.filter(pk=attachment_id).first()
+            if attachment:
+                url = attachment.file.url
+                if url is not None:
+                    pdf = render_to_pdf('view_attachment.html', {
+                        'image': url,
+                    })
+                    if pdf:
+                        response = HttpResponse(pdf, content_type='application/pdf')
+                        filename = "formulario_de_registro.pdf"
+                        content = "inline; filename='{}'" .format(filename)
+                        download = request.GET.get("download")
+                        if download:
+                            content = "attachment; filename='{}'".format(filename)
+                        response['Content-Disposition'] = content
+                        return response
+        return HttpResponse("Not found")
 
 
 class GeneratePDF(View):
