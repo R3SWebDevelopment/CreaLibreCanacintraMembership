bc6dfd83920c6361518599b7439a33e6677bc0f6
Ricardo Tercero <ricardo.tercero@vordem.mx>
2018-04-28 21:53:37 -0500

    Enable pdf file rendering and remove login required decorator

--- contabilidad/membership/views.py
+++ contabilidad/membership/views.py
@@ -3,32 +3,41 @@ from django.http import HttpResponse
 from django.views.generic import View
 from .models import MembershipRequest, AttachedFile
 from utils.render_to_pdf import render_to_pdf
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
 
 
 class ViewAttachmentPDF(View):
 
-    @method_decorator(login_required)
     def get(self, request, id, *args, **kwargs):
         attachment_id = id
         if attachment_id:
             attachment = AttachedFile.objects.filter(pk=attachment_id).first()
             if attachment:
-                url = attachment.file.url
-                if url is not None:
-                    pdf = render_to_pdf('view_attachment.html', {
-                        'image': url,
-                    })
-                    if pdf:
-                        response = HttpResponse(pdf, content_type='application/pdf')
-                        filename = "formulario_de_registro.pdf"
-                        content = "inline; filename='{}'" .format(filename)
-                        download = request.GET.get("download")
-                        if download:
-                            content = "attachment; filename='{}'".format(filename)
-                        response['Content-Disposition'] = content
-                        return response
+                if attachment.file.file.name.lower().endswith(('.pdf', )):
+
+                    response = HttpResponse(attachment.file.file, content_type='application/pdf')
+                    filename = "formulario_de_registro.pdf"
+                    content = "inline; filename='{}'".format(filename)
+                    download = request.GET.get("download")
+                    if download:
+                        content = "attachment; filename='{}'".format(filename)
+                    response['Content-Disposition'] = content
+                    return response
+
+                else:
+                    url = attachment.file.url
+                    if url is not None:
+                        pdf = render_to_pdf('view_attachment.html', {
+                            'image': url,
+                        })
+                        if pdf:
+                            response = HttpResponse(pdf, content_type='application/pdf')
+                            filename = "formulario_de_registro.pdf"
+                            content = "inline; filename='{}'" .format(filename)
+                            download = request.GET.get("download")
+                            if download:
+                                content = "attachment; filename='{}'".format(filename)
+                            response['Content-Disposition'] = content
+                            return response
         return HttpResponse("Not found")
 
 
