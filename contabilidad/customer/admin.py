25e02f385a2567a9bca0844928981bd3443ee999
Ricardo Tercero <ricardo.tercero@vordem.mx>
2018-05-02 23:33:06 -0500

    Add Product and Service model and Certification model to admin site

--- contabilidad/customer/admin.py
+++ contabilidad/customer/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-from .models import Company
+from .models import Company, ProductService, Certification
 
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProductService)
+class ProductServiceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Certification)
+class CertificationAdmin(admin.ModelAdmin):
     pass
