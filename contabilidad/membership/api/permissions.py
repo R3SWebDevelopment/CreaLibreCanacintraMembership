db39baa47ed082a1e8580e6c3cfda9fbe705484c
Ricardo Tercero <ricardo.tercero@vordem.mx>
2018-07-14 15:36:08 -0500

    Improve membership update request permissions

--- contabilidad/membership/api/permissions.py
+++ contabilidad/membership/api/permissions.py
@@ -8,15 +8,13 @@ class MembershipUpdatePermission(permissions.BasePermission):
         is_logged = request.user.is_authenticated()
         is_admin = request.user.is_staff if is_logged else False
 
-        print("is_logged: {}".format(is_logged))
-        print("is_admin: {}".format(is_admin))
-        print("view.action: {}".format(view.action))
-        print("request.method: {}".format(request.method))
-
         if view.action == 'attachments' and is_logged and request.method in ['GET', 'POST']:
             return True
 
         if view.action == 'mine' and is_logged and request.method in ['GET', 'PATCH']:
             return True
 
-        return True if is_logged and is_admin else False
+        is_valid_action = view.action not in ['partial_update', 'retrieve']
+        is_valid_method = request.method in ['GET']
+
+        return True if is_logged and is_admin and is_valid_action and is_valid_method else False
